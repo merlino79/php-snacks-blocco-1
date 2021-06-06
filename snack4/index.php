d93acf6c6b8cc6e5a20ad6fefdaa1edb72a71a73
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-06 15:27:24 +0200

    prove per sistemare le carelle su git

---
+++ snack4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>numeri casuali</title>
+</head>
+<body>
+<h1>numeri casuali salvati in un array</h1>
+<?php
+  for($i=1; $i<15; $i++)echo rand(1,100). " -";
+?>
+  
+</body>
+</html>
