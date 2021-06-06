9330335fd84393d02c5b7daa468a8439abc38186
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-06 15:45:13 +0200

    snack 3

--- snack3/index.php
+++ snack3/index.php
@@ -7,7 +7,70 @@
   <title>sanck3</title>
 </head>
 <body>
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
 <p>snack3</p>
+<?php
+ 
+
+  $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+    var_dump($posts)
+?>
+<ul>
+    <?php foreach($posts as $date => $post){ ?>
+        <li><?php echo $date ?>
+            <ul>
+
+            <?php foreach($post as $messagges) {?>
+                <?php foreach($messagges as $key => $message) {?>
+                    <li><?php echo $key ?> : <?php echo $message ?></li>
+                <?php } ?>
+                <br>
+            <?php } ?>
+
+            </ul>
+        </li>
+        <hr>
+    <?php } ?>
+</ul>
+
   
 </body>
 </html>
