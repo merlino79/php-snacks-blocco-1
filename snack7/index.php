488b72de9acea32cce1fa1e41e33f78a384ad163
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-07 22:15:20 +0200

    stampa con media

--- snack7/index.php
+++ snack7/index.php
@@ -18,22 +18,22 @@
           [
             'lastName' => 'marco',
             'surName' => 'serenello',
-            'voti' => [5,7,8,6,8,],
+            'voti' => [3,7,8,2,8,],
           ],
           [
             'lastName' => 'serena',
             'surName' => 'verdi',
-            'voti' => [5,7,8,4,7,9],
+            'voti' => [5,7,8,4,10,9],
           ],
           [
             'lastName' => 'sara',
             'surName' => 'brambilla',
-            'voti' => [5,7,8,4,7,9],
+            'voti' => [5,7,8,3,7,9],
           ],
           [
             'lastName' => 'franco ',
             'surName' => 'franchi',
-            'voti' => [5,7,8,4,7,9],
+            'voti' => [2,7,8,4,7,9],
           ],
           
           
@@ -45,17 +45,17 @@
 <?php foreach ($students as $studenti) {?>
 
   <li>
-  <?php echo $studenti['lastName'] . ' ' . $studenti['surName']  ?>
+  <?php echo $studenti['lastName'] . ' ' . $studenti['surName'] ?>
   
   
+  
+  </li>
+  <li>
+  media voti:
+  <?php echo number_format(array_sum($studenti['voti']) / count($studenti['voti']), 2, ',', '.' )  ?>
   </li>
 
-  <ul>
-  <li>
-  voti: <?php print_r($studenti['voti'])?>
-  </li>
-    
-  </ul>
+  
   
   
 
